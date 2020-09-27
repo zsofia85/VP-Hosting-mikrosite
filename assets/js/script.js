d2e8e3619358cc2fbaff37e12fbca46e1dbcfe5e
Rose <1083020@ucn.dk>
2020-09-27 13:12:26 +0200

    veganskip

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,3 +6,21 @@ let options = {
 };
 
 videojs('YourHealthyLunch', options);
+
+
+const elmVideo = document.querySelector('#YourHealthyLunch > video');
+
+const elmStatus = document.querySelector('#status');
+elmVideo.addEventListener('play', (event) => {
+    elmStatus.innerHTML = 'Play';
+});
+
+
+
+//Balsamic glazed vegetables button
+const elmJumpTo30 = document.querySelector('#veganskip');
+const funcJumpTo30 = () => {
+    elmVideo.currentTime = 30;
+}
+elmJumpTo20.addEventListener('click', funcJumpTo30);
+
